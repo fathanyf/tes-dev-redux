849fa17b8952d131dd7696cd4433517d92d19a9c
Ashar Ardianto <ashardian.to21@gmail.com>
2022-06-14 22:11:02 +0700

    Gamelist for Label Played

--- src/components/game-components/GameList.js
+++ src/components/game-components/GameList.js
@@ -1,12 +1,49 @@
-import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
+import { getGameList } from '../../store/games/GamesSlice'
+import Link from 'next/link'
 
 const GameList = () => {
+    const data = useSelector((state) => state.games.games)
+    const auth = useSelector((state) => state.user)
+    const dispatch = useDispatch()
+    const [playedGames, setPlayedGames] = useState(false)
+
+    useEffect(() => {
+        dispatch(getGameList(auth.user?.uid))
+    }, [auth.user?.uid, dispatch])
+
+    console.log(data)
+
     return (
         <>
             <section className="feature-section spad">
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-3 col-md-6 p-0">
+                        {
+                            data.map((e) => {
+                                return (
+                                    <div className="col-sm-4" key={e.id}>
+                                        <div className="position-relative mb-5" style={{ maxHeight: '180px', opacity: '0.8' }}>
+                                            <img src={e.imageUrl} alt="Photo 3" className="img-fluid img-games" />
+                                        </div>
+                                        <div className="text-block bg-dark p-2" style={{ opacity: '0.7' }}>
+                                            <h6 className='text-uppercase'>{e.title} <small>({e.release})</small></h6>
+                                            <p>{e.developer} | {e.genre} </p>
+                                            <p>
+                                                <small>{e.description}</small>
+                                            </p>
+                                            {e.isPlayed ? (<center><label>Played!</label></center>) : <center><label>Not Played!</label></center>}
+                                            <Link href={`/games/${e.link}`}>
+                                                <button type="submit" className='btn btn-danger btn-flat btn-block' onClick={() => dispatch(totalReset())}>play now</button>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                )
+
+                            })
+                        }
+                        {/* <div className="col-lg-3 col-md-6 p-0">
                             <div className="feature-item set-bg" data-setbg="/3.jpg" style={{ backgroundImage: 'url("/3.jpg")' }}>
                                 <span className="cata new">strategy</span>
                                 <div className="fi-content text-white">
@@ -45,7 +82,7 @@ const GameList = () => {
                                     <a href="#" className="fi-comment">3 Comments</a>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </section>
